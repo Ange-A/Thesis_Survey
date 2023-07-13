391aa454b314ad6ed36a7213db088b06db95865b
Ange-A <33396595+Ange-A@users.noreply.github.com>
2023-07-13 16:03:25 +0200

    survey

--- survey/survey/__init__.py
+++ survey/survey/__init__.py
@@ -32,19 +32,8 @@ class Player(BasePlayer):
         max=100,
     )
 
-    Preferences = models.StringField(
-        choices=[["Never", "Never"],
-                 ["Sometimes", "Sometimes"],
-                 ["Often", "Often"],
-                 ["Frequently", "Frequently"]],
-
-        label="How often do you buy fast fashion items",
-
-        widget=widgets.RadioSelect,
-    )
-
     Income = models.StringField(
-        label="Please select your monthly income range",
+        label="Please select your monthly income range?",
         choices=[
             ["$0-$500", "$0-$500"],
             ["$500 - $1000", "$500 - $1000"],
@@ -55,14 +44,44 @@ class Player(BasePlayer):
         widget=widgets.RadioSelect,
     )
 
+    Fast = models.StringField(
+        choices=[["Yes", "Yes"],
+                 ["No", "No"]],
+
+        label="Are you aware of the term fast fashion?",
+
+        widget=widgets.RadioSelect,
+    )
+
     Definition = models.StringField(
         choices=[["Quick made garments", "Quick made garments"],
                  ["Affordable garments", "Affordable garments"],
                  ["Trendy garments", "Trendy garments"],
-                 ["Both", "Both"],
+                 ["All the above", "All the above"],
                  ["I have no idea", "I have no idea"]],
 
-        label="How would you describe fast fashion",
+        label="How would you describe fast fashion?",
+
+        widget=widgets.RadioSelect,
+    )
+
+    Preferences = models.StringField(
+        choices=[["Never", "Never"],
+                 ["Sometimes", "Sometimes"],
+                 ["Often", "Often"],
+                 ["Frequently", "Frequently"]],
+
+        label="How often do you buy fast fashion items?",
+
+        widget=widgets.RadioSelect,
+    )
+
+
+    Sus = models.StringField(
+        choices=[["Yes", "Yes"],
+                 ["No", "No"]],
+
+        label="Have you heard of sustainable fast fashion?",
 
         widget=widgets.RadioSelect,
     )
@@ -73,7 +92,7 @@ class Player(BasePlayer):
                  ["Often", "Often"],
                  ["Frequently", "Frequently"]],
 
-        label="Do you think fast fashion brands practice sustainability",
+        label="Do you think fast fashion brands practice sustainability?",
 
         widget=widgets.RadioSelect,
     )
@@ -83,7 +102,7 @@ class Player(BasePlayer):
                  ["Sometimes", "Sometimes"],
                  ["Often", "Often"],
                  ["Frequently", "Frequently"]],
-        label="Do you think brands that call themselves sustainable are actually sustainable",
+        label="Do you think brands that call themselves sustainable are actually sustainable?",
 
         widget=widgets.RadioSelect,
     )
@@ -94,18 +113,22 @@ class Player(BasePlayer):
                  ["Often", "Often"],
                  ["Frequently", "Frequently"]],
 
-        label="With consideration of your budget, would you spend an "
-               "extra 5 dollars to buy a sustainably manufactured fashion item?",
+        label="With consideration of your budget, would you spend an"
+               " extra 5 dollars to buy a sustainably manufactured fashion item?",
 
         widget=widgets.RadioSelect,
     )
 
     Rate = models.StringField(
-        choices=[["Yes", "Yes"],
-                 ["No", "No"]],
-
-        label="On a rate of 1 to 5, how did the previous"
-              " video influence your orientation towards sustainable fast fashion items.",
+        label="On a scale of 1-5, how did the previous video influence"
+              " you to like sustainable fast fashion items? "
+              "(With 1 being meh, and 5 being I am all about sustainability)",
+        choices=[
+            ["1", "1"],
+            ["2", "2"],
+            ["3", "3"],
+            ["4", "4"],
+            ["5", "5"]],
 
         widget=widgets.RadioSelect,
     )
@@ -121,13 +144,27 @@ class Player(BasePlayer):
 
     )
 
+    Recycled = models.StringField(
+        label="On a scale of 1-5, how likely are you to choose a recycled fashion item over a brand new one, "
+              "to promote sustainable fashion?"
+              "(With 1 being I dont care, and 5 being I would not hesitate)",
+        choices=[
+            ["1", "1"],
+            ["2", "2"],
+            ["3", "3"],
+            ["4", "4"],
+            ["5", "5"]],
+
+        widget=widgets.RadioSelect,
+    )
+
     Actually = models.StringField(
         choices=[["Never", "Never"],
                  ["Sometimes", "Sometimes"],
                  ["Often", "Often"],
                  ["Frequently", "Frequently"]],
 
-        label="Do you think brands that call themselves sustainable are  actually sustainable",
+        label="Do you think brands that call themselves sustainable are  actually sustainable?",
 
         widget=widgets.RadioSelect,
     )
@@ -144,7 +181,7 @@ def creating_session(subsession):
 
 class Demographics(Page):
     form_model = 'player'
-    form_fields = ['Title', 'Age', 'Preferences', 'Income', 'Definition']
+    form_fields = ['Title', 'Age', 'Income', 'Fast', 'Definition', 'Preferences', 'Sus']
 
 
 class Sustainability(Page):
@@ -168,7 +205,7 @@ class Intervention1(Page):
 
 class Final(Page):
     form_model = 'player'
-    form_fields = ['Rate', 'Likely', 'Actually']
+    form_fields = ['Rate', 'Likely', 'Recycled', 'Actually']
 
 
 page_sequence = [Demographics, Sustainability, Intervention0, Intervention1, Final]
